e7c22f72e1b5d33947f039ada4f8c04634aa26a3
maakbarofficial <maakbarofficial@gmail.com>
2025-03-12 02:50:43 +0500

    Add title and actions, Add CSS and Add Theme Toggler

--- src/main.js
+++ src/main.js
@@ -1,1 +1,17 @@
-import './style.css'
+import "./style.css";
+
+// Theme Toggler
+function toggleTheme() {
+  const container = document.querySelector(".container");
+  container.classList.toggle("dark-mode");
+
+  const theme = container.classList.contains("dark-mode") ? "dark" : "light";
+  localStorage.setItem("theme", theme);
+}
+
+const container = document.querySelector(".container");
+if (localStorage.getItem("theme") === "dark") {
+  container.classList.add("dark-mode");
+}
+
+document.getElementById("theme-toggle").addEventListener("click", toggleTheme);
